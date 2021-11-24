d6c0f36ff45517a0cf87c1a0be31e3f8a0153661
AndyDiego13 <andyserrano_d@outlook.com>
2021-11-23 20:29:42 -0600

    actividad 5.1

--- Libro.hpp
+++ Libro.hpp
@@ -3,32 +3,52 @@
 
 #include <iostream>
 
-class Libro {
+class Libro
+{
 
     std::string isbn;
     std::string titulo;
     int paginas;
     
-public:
-    Libro()
-    {
-        Libro("sin definir", "sin definir", 0);
-    }
-    
-    Libro(std::string _isbn, std::string _titulo, int _pags) :
-    titulo(_titulo), isbn(_isbn), paginas(_pags)
-    {
-        
-    }
-    
-    ~Libro() {}
-    
-    friend std::ostream & operator<<(std::ostream &os, const Libro &libro)
-    {
-        os << libro.isbn << "-" << libro.titulo << "(" << libro.paginas << ")" << std::endl;
+    public:
 
-        return os;
-    }
+        class Persona
+        {
+            public:
+
+                int key;
+                std::string name;
+                
+                Persona(int _key, std::string _name): 
+                key(_key), name(_name)
+                {
+
+                }
+        }
+        Libro()
+        {
+            Libro("sin definir", "sin definir", 0);
+        }
+    
+        Libro(std::string _isbn, std::string _titulo, int _pags) :
+        titulo(_titulo), isbn(_isbn), paginas(_pags)
+        {
+        
+        }
+    
+        ~Libro() {}
+    
+        friend std::ostream & operator<<(std::ostream &os, const Libro &libro)
+        {
+            os << libro.isbn << "-" << libro.titulo << "(" << libro.paginas << ")" << std::endl;
+
+            return os;
+        }
+
+        friend std::ostream & operator = (std::ostream &os2, const int &key, const std::string &name, const std::string &isbn, const std::vector<Libro> &libro)
+        {
+            os = 
+        }
 };
 
 
--- stlMapMain.cpp
+++ stlMapMain.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <vector>
 #include "Libro.hpp"
 
 //esto es para el reto
@@ -14,71 +15,11 @@ std::map< std::string, int> conexionesPorDia(std::string fecha)
     return conexiones;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-    // Creamos un mapa de personas
-    std::map< int, std::string> personas;
+    std::map< int, std::string, std::string, std::vector<Libro>> registros;
 
-    // Creamos un stl para cada persona que queramos tener
-    std::pair< int, std::string> p1(1,"Maria");
-    // Las insertamos en el mapa
-    personas.insert(p1);
-
-    std::pair< int, std::string> p2(10,"Camila");
-    personas.insert(p2);
-
-    personas[3] = "Raúl";
-
-    for ( auto elem : personas)
-    {
-        std::cout << "Key: " << elem.first << ", Value: " << elem.second << std::endl;
-    }
-
-    std::cout << "=== Busca un valor ===" << std::endl;
-
-    // buscamos una persona creando un mapa iterador
-    std::map< int, std::string>::iterator it;
-    it = personas.find(20);
-
-    if (it == personas.end())
-    {
-        std::cout << "La persona buscada no se encontró" << std::endl;
-    }
-    else
-    {
-        std::cout << "La persona buscada es: " << it->second << std::endl;
-    }
-
-    std::cout << "=== Mapa de Libros ===" << std::endl;
-
-    // Creamos un mapa de libros
-    std::map< std::string, Libro> libros;
-
-    // Le asignamos la key y el contenido con el constructor de libros
-    libros["5"] = Libro("5", "El señor de los anillos", 700);
-    libros["2"] = Libro("2", "El capital", 550);
-    libros["3"] = Libro("3", "El arte de la guerra", 345);
-
-    for( auto elem : libros)
-    {
-        std::cout << "ISBN: " << elem.first << ", Libro: " << elem.second << std::endl;
-    } 
-
-    std::cout << "=== Buscar un valor ===" << std::endl;
-
-    // Creamos un segundo iterador para ahora encontrar los libros
-    std::map< std::string, Libro>::iterator it2;
-
-    it2 = libros.find("3");
-
-    if (it2 == libros.end())
-    {
-        std::cout << "El libro no se encontró" << std::endl;
-    }
-    else
-    {
-        std::cout << "El libro es: " << it2->second << std::endl;
-    }
+    registros = (1,"Maria", "5", Libro("5","El señor de los anillos", 700);
 
     /* Mapa con una estructura lineal */
     // std::map< std::string, std::vector<Libro>> combinado;
